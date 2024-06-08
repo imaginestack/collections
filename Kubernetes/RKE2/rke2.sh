e28d418d1c1f68a14f441241adc294c0b501762b
ImagineStack <support@imaginestack.io>
2024-06-08 06:12:10 -0700

    Update rke2.sh

--- Kubernetes/RKE2/rke2.sh
+++ Kubernetes/RKE2/rke2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 
 #############################################
 # YOU SHOULD ONLY NEED TO EDIT THIS SECTION #
@@ -34,25 +35,26 @@ all=($master1 $master2 $master3)
 # Array of all minus master1
 allnomaster1=($master2 $master3 )
 
-#Loadbalancer IP range
+# Loadbalancer IP range
 lbrange=192.168.3.60-192.168.3.80
 
-#ssh certificate name variable
+# SSH certificate name variable
 certName=id_ed25519.3
 
 #############################################
 #            DO NOT EDIT BELOW              #
 #############################################
-# For testing purposes - in case time is wrong due to VM snapshots
+
+# Ensure NTP is set
 sudo timedatectl set-ntp off
 sudo timedatectl set-ntp on
 
-# Move SSH certs to ~/.ssh and change permissions
+# Ensure SSH certs are in place
 chmod 600 /home/$user/.ssh/$certName 
 chmod 644 /home/$user/.ssh/$certName.pub
 
-# Install Kubectl if not already present
-if ! command -v kubectl version &> /dev/null
+# Install kubectl if not present
+if ! command -v kubectl &> /dev/null
 then
     echo -e " \033[31;5mKubectl not found, installing\033[0m"
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -61,106 +63,111 @@ else
     echo -e " \033[32;5mKubectl already installed\033[0m"
 fi
 
-# Create SSH Config file to ignore checking (don't use in production!)
-sed -i '1s/^/StrictHostKeyChecking no\n/' ~/.ssh/config
+# Create SSH config to ignore host key checking (not recommended for production)
+if ! grep -Fxq "StrictHostKeyChecking no" ~/.ssh/config
+then
+    echo "StrictHostKeyChecking no" | sudo tee -a ~/.ssh/config
+fi
 
-#add ssh keys for all nodes
+# Add SSH keys for all nodes
 for node in "${all[@]}"; do
-  ssh-copy-id $user@$node
+  ssh-copy-id -i /home/$user/.ssh/$certName.pub $user@$node
 done
 
-# Step 1: Create Kube VIP
-# create RKE2's self-installing manifest dir
+# Create RKE2 manifest directory and Kube-VIP manifest
 sudo mkdir -p /var/lib/rancher/rke2/server/manifests
-# Install the kube-vip deployment into rke2's self-installing manifest folder
 curl -sO https://raw.githubusercontent.com/imaginestack/collections/main/Kubernetes/RKE2/kube-vip
-cat kube-vip | sed 's/$interface/'$interface'/g; s/$vip/'$vip'/g' > $HOME/kube-vip.yaml
-sudo mv kube-vip.yaml /var/lib/rancher/rke2/server/manifests/kube-vip.yaml
+sed 's/$interface/'$interface'/g; s/$vip/'$vip'/g' kube-vip > $HOME/kube-vip.yaml
+sudo mv $HOME/kube-vip.yaml /var/lib/rancher/rke2/server/manifests/kube-vip.yaml
 
-# Find/Replace all k3s entries to represent rke2
+# Update Kube-VIP manifest for RKE2
 sudo sed -i 's/k3s/rke2/g' /var/lib/rancher/rke2/server/manifests/kube-vip.yaml
-# copy kube-vip.yaml to home directory
-sudo cp /var/lib/rancher/rke2/server/manifests/kube-vip.yaml ~/kube-vip.yaml
-# change owner
-sudo chown $user:$user kube-vip.yaml
-# make kube folder to run kubectl later
-mkdir ~/.kube
 
-# create the rke2 config file
+# Create RKE2 config file
 sudo mkdir -p /etc/rancher/rke2
-touch config.yaml
-echo "tls-san:" >> config.yaml 
-echo "  - $vip" >> config.yaml
-echo "  - $master1" >> config.yaml
-echo "  - $master2" >> config.yaml
-echo "  - $master3" >> config.yaml
-echo "write-kubeconfig-mode: 0644" >> config.yaml
-echo "disable:" >> config.yaml
-echo "  - rke2-ingress-nginx" >> config.yaml
-# copy config.yaml to rancher directory
-sudo cp ~/config.yaml /etc/rancher/rke2/config.yaml
+cat <<EOF | sudo tee /etc/rancher/rke2/config.yaml
+tls-san:
+  - $vip
+  - $master1
+  - $master2
+  - $master3
+write-kubeconfig-mode: 0644
+disable:
+  - rke2-ingress-nginx
+EOF
 
-# update path with rke2-binaries
-echo 'export KUBECONFIG=/etc/rancher/rke2/rke2.yaml' >> ~/.bashrc ; echo 'export PATH=${PATH}:/var/lib/rancher/rke2/bin' >> ~/.bashrc ; echo 'alias k=kubectl' >> ~/.bashrc ; source ~/.bashrc ;
+# Update bash profile for kubectl and RKE2
+if ! grep -Fxq "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" ~/.bashrc
+then
+    echo 'export KUBECONFIG=/etc/rancher/rke2/rke2.yaml' >> ~/.bashrc
+    echo 'export PATH=${PATH}:/var/lib/rancher/rke2/bin' >> ~/.bashrc
+    echo 'alias k=kubectl' >> ~/.bashrc
+    source ~/.bashrc
+fi
 
-# Step 2: Copy kube-vip.yaml and certs to all masters
+# Copy kube-vip.yaml and SSH certs to all master nodes
 for newnode in "${allmasters[@]}"; do
   scp -i ~/.ssh/$certName $HOME/kube-vip.yaml $user@$newnode:~/kube-vip.yaml
-  scp -i ~/.ssh/$certName $HOME/config.yaml $user@$newnode:~/config.yaml
+  scp -i ~/.ssh/$certName /etc/rancher/rke2/config.yaml $user@$newnode:~/config.yaml
   scp -i ~/.ssh/$certName ~/.ssh/{$certName,$certName.pub} $user@$newnode:~/.ssh
-  echo -e " \033[32;5mCopied successfully!\033[0m"
+  echo -e " \033[32;5mCopied successfully to $newnode\033[0m"
 done
 
-# Step 3: Connect to Master1 and move kube-vip.yaml and config.yaml. Then install RKE2, copy token back to admin machine. We then use the token to bootstrap additional masternodes
-ssh -tt $user@$master1 -i ~/.ssh/$certName sudo su <<EOF
-mkdir -p /var/lib/rancher/rke2/server/manifests
-mv kube-vip.yaml /var/lib/rancher/rke2/server/manifests/kube-vip.yaml
-mkdir -p /etc/rancher/rke2
-mv config.yaml /etc/rancher/rke2/config.yaml
-echo 'export KUBECONFIG=/etc/rancher/rke2/rke2.yaml' >> ~/.bashrc ; echo 'export PATH=${PATH}:/var/lib/rancher/rke2/bin' >> ~/.bashrc ; echo 'alias k=kubectl' >> ~/.bashrc ; source ~/.bashrc ;
-curl -sfL https://get.rke2.io | sh -
-systemctl enable rke2-server.service
-systemctl start rke2-server.service
-echo "StrictHostKeyChecking no" > ~/.ssh/config
-ssh-copy-id -i /home/$user/.ssh/$certName $user@$admin
-scp -i /home/$user/.ssh/$certName /var/lib/rancher/rke2/server/token $user@$admin:~/token
-scp -i /home/$user/.ssh/$certName /etc/rancher/rke2/rke2.yaml $user@$admin:~/.kube/rke2.yaml
+# Connect to master1, install RKE2, and start the server
+ssh -tt $user@$master1 -i ~/.ssh/$certName <<EOF
+sudo mv ~/kube-vip.yaml /var/lib/rancher/rke2/server/manifests/kube-vip.yaml
+sudo mv ~/config.yaml /etc/rancher/rke2/config.yaml
+echo 'export KUBECONFIG=/etc/rancher/rke2/rke2.yaml' >> ~/.bashrc
+echo 'export PATH=${PATH}:/var/lib/rancher/rke2/bin' >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+source ~/.bashrc
+curl -sfL https://get.rke2.io | sudo sh -
+sudo systemctl enable rke2-server.service
+sudo systemctl start rke2-server.service
+sleep 30
+if sudo systemctl is-active --quiet rke2-server.service; then
+    echo -e " \033[32;5mRKE2 server is running on $master1\033[0m"
+else
+    echo -e " \033[31;5mRKE2 server failed to start on $master1\033[0m"
+    exit 1
+fi
+scp -i /home/$user/.ssh/$certName /var/lib/rancher/rke2/server/token $user@$master1:~/token
+scp -i /home/$user/.ssh/$certName /etc/rancher/rke2/rke2.yaml $user@$master1:~/.kube/rke2.yaml
 exit
 EOF
-echo -e " \033[32;5mMaster1 Completed\033[0m"
 
-# Step 4: Set variable to the token we just extracted, set kube config location
-token=`cat token`
-sudo cat ~/.kube/rke2.yaml | sed 's/127.0.0.1/'$master1'/g' > $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-export KUBECONFIG=${HOME}/.kube/config
-sudo cp ~/.kube/config /etc/rancher/rke2/rke2.yaml
-kubectl get nodes
+# Export the token
+token=$(ssh -i ~/.ssh/$certName $user@$master1 "cat ~/token")
 
-# Step 5: Install kube-vip as network LoadBalancer - Install the kube-vip Cloud Provider
-kubectl apply -f https://kube-vip.io/manifests/rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/kube-vip/kube-vip-cloud-provider/main/manifest/kube-vip-cloud-controller.yaml
-
-# Step 6: Add other Masternodes, note we import the token we extracted from step 3
+# Join other master nodes to the cluster
 for newnode in "${masters[@]}"; do
-  ssh -tt $user@$newnode -i ~/.ssh/$certName sudo su <<EOF
-  mkdir -p /etc/rancher/rke2
-  touch /etc/rancher/rke2/config.yaml
-  echo "token: $token" >> /etc/rancher/rke2/config.yaml
-  echo "server: https://$master1:9345" >> /etc/rancher/rke2/config.yaml
-  echo "tls-san:" >> /etc/rancher/rke2/config.yaml
-  echo "  - $vip" >> /etc/rancher/rke2/config.yaml
-  echo "  - $master1" >> /etc/rancher/rke2/config.yaml
-  echo "  - $master2" >> /etc/rancher/rke2/config.yaml
-  echo "  - $master3" >> /etc/rancher/rke2/config.yaml
-  curl -sfL https://get.rke2.io | sh -
-  systemctl enable rke2-server.service
-  systemctl start rke2-server.service
+  ssh -tt $user@$newnode -i ~/.ssh/$certName <<EOF
+  sudo mv ~/config.yaml /etc/rancher/rke2/config.yaml
+  echo "token: $token" | sudo tee -a /etc/rancher/rke2/config.yaml
+  echo "server: https://$vip:9345" | sudo tee -a /etc/rancher/rke2/config.yaml
+  echo 'export KUBECONFIG=/etc/rancher/rke2/rke2.yaml' >> ~/.bashrc
+  echo 'export PATH=${PATH}:/var/lib/rancher/rke2/bin' >> ~/.bashrc
+  echo 'alias k=kubectl' >> ~/.bashrc
+  source ~/.bashrc
+  curl -sfL https://get.rke2.io | sudo sh -
+  sudo systemctl enable rke2-server.service
+  sudo systemctl start rke2-server.service
+  sleep 30
+  if sudo systemctl is-active --quiet rke2-server.service; then
+      echo -e " \033[32;5mRKE2 server is running on $newnode\033[0m"
+  else
+      echo -e " \033[31;5mRKE2 server failed to start on $newnode\033[0m"
+      exit 1
+  fi
   exit
 EOF
-  echo -e " \033[32;5mMaster node joined successfully!\033[0m"
 done
 
+# Set correct permissions for the directory and files
+sudo chown -R cto:cto /var/lib/rancher/rke2/server/tls/
+sudo chmod -R 750 /var/lib/rancher/rke2/server/tls/
+
+# Verify the nodes
 kubectl get nodes
 
 # Step 8: Install Metallb
